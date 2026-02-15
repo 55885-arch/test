4180b32d92337dfd58cc47cf5e8b0dd68b3f9e70
55885-arch <55885@yupparaj.ac.th>
2026-02-15 11:58:01 +0700

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,4 +1,6 @@
 export default function handler(req, res) {
   const client_id = process.env.CLIENT_ID;
-  res.redirect(`https://github.com/login/oauth/authorize?client_id=${client_id}&scope=repo`);
+  res.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${client_id}&scope=repo`
+  );
 }
